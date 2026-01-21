93a909dfa80ec13cf0d62eaca815e18ae63db516
sigmdel <michel@sigmdel.ca>
2026-01-21 14:35:48 -0400

    Standard .ino file for this repo

--- 06_wifi/02_wifi_connect/wifi_connect/wifi_connect.ino
+++ 06_wifi/02_wifi_connect/wifi_connect/wifi_connect.ino
@@ -1,9 +1,27 @@
-/* Wi-Fi STA Connect and Disconnect Example
+/*
+ * wifi_connect.ino
+ * Connect to 2.4GHz or 5GHz band Wi-Fi network with XIAO ESP32C5 using BSSID.
+ *
+ * This is a stub to satisfy the Arduino IDE, the source code is in
+ * the file main.cpp in the same directory.
+ *
+ * This sketch will compile in the Arduino IDE
+ *
+ *  1- Add https://espressif.github.io/arduino-esp32/package_esp32_index.json
+ *     in the Additional Boards Manager URLS in the Preferences window.
+ *  2- Install platform esp32 by Espressif version 3.3.4 or newer with the Boards Manager
+ *  3- Select the XIAO_ESP32C5 board
+ *
+ * Reference:
+ *   WiFiClientConnect.ino in esp32/3xxx/libraries/WiFi/examples
+ *
+ * Michel Deslierres
+ * January 20, 2026
+ *
+ * Copyright 2026, Michel Deslierres. No rights reserved, this code is in the public domain.
+ * In those jurisdictions where this may be a problem, the BSD Zero Clause License applies.
+ * <https://spdx.org/licenses/0BSD.html> */
+ // SPDX-License-Identifier: 0BSD
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
 
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
 
-*/
